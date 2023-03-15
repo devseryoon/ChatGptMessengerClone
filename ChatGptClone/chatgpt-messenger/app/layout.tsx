224bdd5218c2db1c52d347f927e9c5fc509de0ca
devseryoon <sphere.sryn@gmail.com>
2023-03-15 22:53:47 +0900

    1시간 5분 SessionProvider설정까지

--- ChatGptClone/chatgpt-messenger/app/layout.tsx
+++ ChatGptClone/chatgpt-messenger/app/layout.tsx
@@ -1,28 +1,33 @@
 import SideBar from "../components/SideBar";
 import "../styles/globals.css";
-
+import { SessionProvider } from "../components/SessionProvider";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../pages/api/auth/[...nextAuth]";
 //얘는 이 전체 프로젝트에 근간이 되는 레이아웃임.
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
-    <html lang="en">
+    <html>
       <head />
       <body>
-        <div className="flex">
-          {/** Side bar */}
-          <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
-            <SideBar />
+        <SessionProvider session={session}>
+          <div className="flex">
+            {/** Side bar */}
+            <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+              <SideBar />
+            </div>
+
+            {/** ClientProvider - Notification
+             * 이건 리액트 토스트 쓰는거를 여기다 넣을 거임
+             */}
+
+            <div className="bg-[#343541] flex-1"> {children}</div>
           </div>
-
-          {/** ClientProvider - Notification
-           * 이건 리액트 토스트 쓰는거를 여기다 넣을 거임
-           */}
-
-          <div className="bg-[#343541] flex-1"> {children}</div>
-        </div>
+        </SessionProvider>
       </body>
     </html>
   );
