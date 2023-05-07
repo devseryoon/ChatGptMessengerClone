786b03d7f70dd8c0efa161d32cee6ecc2fc6c041
devseryoon <sphere.sryn@gmail.com>
2023-05-07 16:38:52 +0900

    _

--- ChatGptClone/chatgpt-messenger/components/Login.tsx
+++ ChatGptClone/chatgpt-messenger/components/Login.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Login = () => {
   return (
-    <div>
+    <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
       <Image
         src="https://links.papareact.com/2i6"
         width={300}
@@ -12,7 +12,15 @@ const Login = () => {
         alt="logo"
       />
       {/* 이미지 컴포넌트 컨피그제이에스가서 도메인 우리가 신뢰할 수 있는 적용해놔야 함. */}
-      <button> Sign In to use ChatGPT</button>
+      <button
+        onClick={() => {
+          signIn("google");
+        }}
+        className="text-white font-bold text-3xl animate-pulse"
+      >
+        {" "}
+        Sign In to use ChatGPT
+      </button>
     </div>
   );
 };
